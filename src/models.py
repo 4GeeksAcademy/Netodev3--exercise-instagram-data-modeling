e0a46ad52ee00648be3d0169f11b38c22336a9f9
Net Stark <105382120+Netodev3@users.noreply.github.com>
2024-03-18 22:03:49 +0000

    Problema con Enum

--- src/models.py
+++ src/models.py
@@ -4,31 +4,56 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from enum import Enum
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(20), nullable=False, unique=True)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(20), nullable=False)
+    email = Column(String(20), nullable=False, unique=True)
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
+class Follower(Base):
+    __tablename__ = 'follower'
+    ID = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.ID'))
+    user_from_id_relationship = relationship(User)
+    user_to_id = Column(Integer, ForeignKey('user.ID'))
+    user_to_id_relationship = relationship(User)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    user_id_relationship = relationship(User)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String(200))
+    author_id = Column(Integer, ForeignKey('user.ID'))
+    author_id_relationship = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    post_id_relationship = relationship(Post)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    ID = Column(Integer, primary_key=True)
+    type = Column(Enum)
+    url = Column(String(30))
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    post_id_relationship = relationship(Post)
+
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
